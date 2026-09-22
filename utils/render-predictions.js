31897344e33156c6eba25d573413dcfd8e532071
Shivani Barman <shivanibarman247@gmail.com>
2026-09-22 21:57:31 +0530

    Refactor code structure for improved readability and maintainability

--- utils/render-predictions.js
+++ utils/render-predictions.js
@@ -1,8 +1,10 @@
+import { throttle } from "lodash";
+
 export const renderPredictions = (predictions, ctx) => {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+  // Fonts
   const font = "16px sans-serif";
-
   ctx.font = font;
   ctx.textBaseline = "top";
 
@@ -11,22 +13,31 @@ export const renderPredictions = (predictions, ctx) => {
 
     const isPerson = prediction.class === "person";
 
+    // bounding box
     ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
     ctx.lineWidth = 4;
-
     ctx.strokeRect(x, y, width, height);
 
-    ctx.fillStyle = `rgba(255,0,0,${isPerson ? 0.2 : 0})`;
+    // fill the color
+    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; // Set the fill color to red
     ctx.fillRect(x, y, width, height);
 
+    // Draw the label background.
     ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
-
     const textWidth = ctx.measureText(prediction.class).width;
-    const textHeight = parseInt(font, 10);
-
+    const textHeight = parseInt(font, 10); // base 10
     ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
 
     ctx.fillStyle = "#000000";
     ctx.fillText(prediction.class, x, y);
+
+    if (isPerson) {
+      playAudio();
+    }
   });
 };
+
+const playAudio = throttle(() => {
+  const audio = new Audio("/pols-aagyi-pols.mp3");
+  audio.play();
+}, 2000);
